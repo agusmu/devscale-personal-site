850777a00c3d8dc17122be964c4b525127e43b19
Agus Muhammad <agusmu7@gmail.com>
2023-09-10 02:13:09 +0700

    personal site v1

---
+++ src/javascript/script.js
@@ -0,0 +1,9 @@
+import Typed from "typed.js";
+
+new Typed("#heading", {
+  strings: ["Digital Product Creator", "WordPress and AI Educator", "Digital Marketer"],
+  typeSpeed: 78,
+  backSpeed: 50,
+  loop: true,
+  cursorChar: "_",
+});
